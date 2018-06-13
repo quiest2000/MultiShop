f9b6633885a3c6912921eb43f51606b99404c470
DESKTOP-88NKC77\quinn <quiest2000@gmail.com>
2018-06-13 07:10:11 +0700

    init

---
+++ MultiShop/Models/EShopV20/MultiShopDbContext.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    
+    public partial class MultiShopDbContext : DbContext
+    {
+        public MultiShopDbContext() : base("name=MultiShop")
+        {
+           Database.SetInitializer<MultiShopDbContext>(new CreateDatabaseIfNotExists<MultiShopDbContext>());
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+    }
